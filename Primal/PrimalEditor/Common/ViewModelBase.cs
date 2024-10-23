35301dc46f2a8922d94f92bc76f122d4b6d43ae7
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2024-10-23 15:47:17 -0400

    Cooper Push
    -Added Selectable project templates
    -Added Serailizer for data serialization and deserialization
    -Created project templates folder with 4 project templates
    -Modified XMAL to show an icon and a screen shot whenever you select a project template

---
+++ Primal/PrimalEditor/Common/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace PrimalEditor.GameProject
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
